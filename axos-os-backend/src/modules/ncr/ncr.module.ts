804e07391769c2006eb01090c50ebc4dc81c8ae5
Sergio Valle <sergiovalle3121@gmail.com>
2026-04-23 17:48:17 -0600

    Fix: Added missing GovernanceModule import to NcrModule to resolve runtime dependency injection error.

--- axos-os-backend/src/modules/ncr/ncr.module.ts
+++ axos-os-backend/src/modules/ncr/ncr.module.ts
@@ -4,11 +4,13 @@ import { NCR } from './entities/ncr.entity';
 import { NcrService } from './ncr.service';
 import { NcrController } from './ncr.controller';
 import { EventLedgerModule } from '../event-ledger/event-ledger.module';
+import { GovernanceModule } from '../governance/governance.module';
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([NCR]),
     EventLedgerModule,
+    GovernanceModule,
   ],
   controllers: [NcrController],
   providers: [NcrService],
