8a761c7c0b8bff422da70cad73f8396b3bb025be
Sergio Valle <sergiovalle3121@gmail.com>
2026-04-23 11:26:42 -0600

    fix(suppliers): include IQCInspection repository in SuppliersModule

--- axos-os-backend/src/modules/suppliers/suppliers.module.ts
+++ axos-os-backend/src/modules/suppliers/suppliers.module.ts
@@ -4,12 +4,13 @@ import { Supplier } from './entities/supplier.entity';
 import { SCAR } from './entities/scar.entity';
 import { SuppliersService } from './suppliers.service';
 import { SuppliersController } from './suppliers.controller';
+import { IQCInspection } from '../quality/entities/iqc-inspection.entity';
 import { EventLedgerModule } from '../event-ledger/event-ledger.module';
 import { GovernanceModule } from '../governance/governance.module';
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([Supplier, SCAR]),
+    TypeOrmModule.forFeature([Supplier, SCAR, IQCInspection]),
     EventLedgerModule,
     GovernanceModule
   ],
